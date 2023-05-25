58a0d02df61e6bad85caf886a29b02bb1ad41916
redbeanmk765 <127715673+redbeanmk765@users.noreply.github.com>
2023-05-26 00:07:55 +0900

    monster

---
+++ My project/Assets/sciptableObjects/Monster.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Monster")]
+
+public class Monster : ScriptableObject
+{
+   
+    
+    
+}
