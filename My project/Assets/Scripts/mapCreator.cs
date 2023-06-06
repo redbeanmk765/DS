0f75e3605cc4f86672a21df9ddddb81c1694a204
redbeanmk765 <127715673+redbeanmk765@users.noreply.github.com>
2023-06-06 23:58:54 +0900

    randomMap
    10week

--- My project/Assets/Scripts/mapCreator.cs
+++ My project/Assets/Scripts/mapCreator.cs
@@ -3,55 +3,159 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.Linq;
+using System.Text;
 
 
 public class mapCreator : MonoBehaviour
 {
-
-    public Tilemap Map;
-    public Tilemap Wall;
+    public Rooms rooms;
+    public WallType wallType;
+    public GameObject Map;
     public TileBase floorTile;
-
-    public TileBase ceilingTile;
+    public TileBase ceiling2Tile;
     public TileBase wallTile;
     public TileBase wall2Tile;
+    static List<Room> roomList = new List<Room>();
+  
 
     public int RoomX;
     public int RoomY;
     // Start is called before the first frame update
     void Start()
-    {      
+    {
+        for (int x = -80; x < 80; x++)
+        {
+            for (int y = -54; y < 54; y++)
+            {
+                Map.gameObject.GetComponent<Tilemap>().SetTile(new Vector3Int(RoomX + x, RoomY + y, 0), ceiling2Tile);
+            }
+        }
+
         for (int x = -8; x < 8; x++)
         {
             for (int y = -6; y < 6; y++)
             {
-                Map.SetTile(new Vector3Int(RoomX + x, RoomY + y, 0), floorTile);
+                Map.gameObject.GetComponent<Tilemap>().SetTile(new Vector3Int(RoomX + x, RoomY + y, 0), floorTile);
             }
         }
+        CreateDungeon();
 
-        for (int y = -6; y < 6; y++)
+
+
+        for (int i = 0; i < roomList.Count; ++i)
         {
+            GameObject TmpRoom;
+            TmpRoom = Instantiate(rooms.EasyRooms[Random.Range(0, rooms.EasyRooms.Count)]);
+            TmpRoom.transform.position = new Vector3Int(roomList[i].roomPos.x + 250, roomList[i].roomPos.y + 125, 0);
 
-            Wall.SetTile(new Vector3Int(RoomX + -8 , RoomY + y, 0), ceilingTile);
-            Wall.SetTile(new Vector3Int(RoomX + -7, RoomY + y, 0), ceilingTile);
-            Wall.SetTile(new Vector3Int(RoomX + 6, RoomY + y, 0), ceilingTile);
-            Wall.SetTile(new Vector3Int(RoomX + 7, RoomY + y, 0), ceilingTile);
+            for (int x = -8; x < 8; x++)
+            {
+                for (int y = -6; y < 6; y++)
+                {
+                    Map.gameObject.GetComponent<Tilemap>().SetTile(new Vector3Int(roomList[i].roomPos.x + x + 250, roomList[i].roomPos.y + y + 125, 0), floorTile);
+                }
+               
+            }
 
+            switch (roomList[i].dir) {
+                
+                case "URDL":
+                    TmpRoom = Instantiate(wallType.Wall_URDL);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                        break;
+
+
+                case "URD":
+                    TmpRoom = Instantiate(wallType.Wall_URD);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+                case "URL":
+                     TmpRoom = Instantiate(wallType.Wall_URL);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+                case "UDL":
+                     TmpRoom = Instantiate(wallType.Wall_UDL);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+                case "RDL":
+                    TmpRoom = Instantiate(wallType.Wall_RDL);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+
+                case "UR":
+                    TmpRoom = Instantiate(wallType.Wall_UR);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+
+                case "UD":
+                    TmpRoom = Instantiate(wallType.Wall_UD);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+
+                case "UL":
+                    TmpRoom = Instantiate(wallType.Wall_UL);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+                case "RD":
+                    TmpRoom = Instantiate(wallType.Wall_RD);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+                case "RL":
+                    TmpRoom = Instantiate(wallType.Wall_RL);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+                case "DL":
+                    TmpRoom = Instantiate(wallType.Wall_DL);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+
+                case "U":
+                    TmpRoom = Instantiate(wallType.Wall_U);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+                case "R":
+                    TmpRoom = Instantiate(wallType.Wall_R);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+                case "D":
+                    TmpRoom = Instantiate(wallType.Wall_D);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+                case "L":
+                    TmpRoom = Instantiate(wallType.Wall_L);
+                    TmpRoom.transform.SetParent(Map.transform);
+                    TmpRoom.transform.position = roomList[i].roomPos;
+                    break;
+
+            }
         }
-
-        for (int x = -6; x < 6; x++)
-        {
-
-            Wall.SetTile(new Vector3Int(RoomX + x, RoomY + -6, 0), ceilingTile);
-            Wall.SetTile(new Vector3Int(RoomX + x, RoomY + -5, 0), ceilingTile);
-            Wall.SetTile(new Vector3Int(RoomX + x, RoomY + 4, 0), wallTile);
-            Wall.SetTile(new Vector3Int(RoomX + x, RoomY + 5, 0), ceilingTile);
-
-        }
-
-        
-
-
     }
 
     // Update is called once per frame
@@ -65,22 +169,51 @@ public class mapCreator : MonoBehaviour
     static int[] neighborNum = new int[100];
     static int[] room = new int[100];
     static int[] endRooms = new int[100];
-    static Queue<int> endRoomsNum = new Queue<int>();
+    static List<int> endRoomsNum = new List<int>();
     static public int steps;
     static public int boosRoomStep;
     static public int boosRoomNum;
+    
 
-    static void CreateMap(string[] args)
+    public enum Diff
+    {
+        Chest,
+        Shop,
+        Boss,
+        Easy,
+        Norm,
+        Hard,
+        Start
+    }
+    public class Room
+    {
+        public int roomNum;
+        public Vector3Int roomPos;
+        public string dir;
+        public Diff roomDiff;
+
+        public Room(int num, string direction, Diff diff)
+        {
+            roomNum = num;
+            dir = direction;
+            roomDiff = diff;
+            roomPos = new Vector3Int ( (((num % 10) - 5) * 16) - 250, (((num / 10) - 4) * -12 ) - 125, 0);
+
+
+        }
+
+    }
+    static void CreateDungeon()
     {
 
         int startRoom = 45;
-        levelRooms = 15;
+        levelRooms = 9;
 
 
         while (true)
         {
             visit(room, startRoom);
-            if (madeRooms > levelRooms && endRoomsNum.Count > 5)
+            if (madeRooms > levelRooms && endRoomsNum.Count > 3 && neighborNum[45] >= 2)
             {
                 break;
             }
@@ -89,6 +222,7 @@ public class mapCreator : MonoBehaviour
                 room[i] = 0;
                 neighborNum[i] = 0;
                 endRooms[i] = 0;
+                
             }
             madeRooms = 0;
             endRoomsNum.Clear();
@@ -97,12 +231,123 @@ public class mapCreator : MonoBehaviour
             boosRoomNum = 0;
         }
 
+        endRoomsNum = ShuffleList<int>(endRoomsNum);
+
+
+        for (int i = 0; i < endRoomsNum.Count(); i++)
+        {
+            if (endRoomsNum[i] == boosRoomNum)
+            {
+                endRoomsNum.RemoveAt(i);
+            }
+
+        }
+
+        int chestNum = endRoomsNum[0];
+        int shopNum = endRoomsNum[1];
+
         for (int i = 0; i <= 9; i++)
         {
             for (int j = 0; j <= 9; j++)
-            { 
+            {
+                if (room[i * 10 + j] == 1)
+                {
+                    int num = i * 10 + j;
+
+                    StringBuilder dirB = new StringBuilder();
+                    Diff tmpDiff = Diff.Easy;
+
+                    if (room[num - 10] == 1)
+                    {
+                        dirB.Append("U");
+                    }
+                    if (room[num + 1] == 1)
+                    {
+                        dirB.Append("R");
+                    }
+                    if (room[num + 10] == 1)
+                    {
+                        dirB.Append("D");
+                    }
+                    if (room[num - 1] == 1)
+                    {
+                        dirB.Append("L");
+                    }
+
+                    if (num == chestNum)
+                    {
+                        tmpDiff = Diff.Chest;
+                    }
+                    else if (num == shopNum)
+                    {
+                        tmpDiff = Diff.Shop;
+                    }
+                    else if (num == boosRoomNum)
+                    {
+                        tmpDiff = Diff.Boss;
+                    }
+                    else if (num == 45)
+                    {
+                        tmpDiff = Diff.Start;
+                    }
+                    else
+                    {
+                        int chance = Random.Range(0, 4);
+
+                        switch (chance)
+                        {
+                            case 0:
+                                tmpDiff = Diff.Hard;
+                                break;
+                            case 1:
+                            case 2:
+                                tmpDiff = Diff.Norm;
+                                break;
+                            case 3:
+                                tmpDiff = Diff.Easy;
+                                break;
+                        }
+                    }
+                    string dir = dirB.ToString();
+
+                    roomList.Add(new Room(i * 10 + j, dir, tmpDiff));
+                }
+
             }
         }
+
+        for (int i = 0; i <= 9; i++)
+        {
+            for (int j = 0; j <= 9; j++)
+            {
+               
+
+
+            }
+
+        }
+
+
+        for (int i = 0; i < roomList.Count(); i++)
+        {
+
+        
+
+           
+
+        }
+
+        for (int i = 0; i < endRoomsNum.Count(); i++)
+        {
+
+         
+            
+
+        }
+
+
+        
+  
     }
 
     static public void visit(int[] room, int roomNum)
@@ -117,7 +362,7 @@ public class mapCreator : MonoBehaviour
                 return;
 
             }
-            int chance = Random.Range(0,2);
+            int chance = Random.Range(0, 2);
             if (roomNum != 45)
                 if (chance == 1)
                 {
@@ -129,6 +374,9 @@ public class mapCreator : MonoBehaviour
 
             room[roomNum] = 1;
             madeRooms++;
+
+
+
 
             int[] nextRoomNum = new int[4];
             int[] dir = { 0, 1, 2, 3 };
@@ -143,8 +391,9 @@ public class mapCreator : MonoBehaviour
                 neighborNum[nextRoomNum[i]]++;
             }
 
+
             int tmp;
-            
+
             for (int i = 0; i < dir.Length; ++i)
             {
                 int random1 = UnityEngine.Random.Range(0, dir.Length);
@@ -154,7 +403,6 @@ public class mapCreator : MonoBehaviour
                 dir[random1] = dir[random2];
                 dir[random2] = tmp;
             }
-
 
 
             for (int i = 0; i <= 3; i++)
@@ -170,7 +418,7 @@ public class mapCreator : MonoBehaviour
 
             if (endRooms[roomNum] == 1)
             {
-                endRoomsNum.Enqueue(roomNum);
+                endRoomsNum.Add(roomNum);
                 if (boosRoomStep <= steps)
                 {
                     boosRoomStep = steps;
@@ -182,4 +430,24 @@ public class mapCreator : MonoBehaviour
         return;
 
     }
+
+    static public List<T> ShuffleList<T>(List<T> list)
+    {
+        int random1, random2;
+        T temp;
+
+        for (int i = 0; i < list.Count; ++i)
+        {
+            random1 = Random.Range(0, list.Count);
+            random2 = Random.Range(0, list.Count);
+
+            temp = list[random1];
+            list[random1] = list[random2];
+            list[random2] = temp;
+        }
+
+        return list;
+    }
+
 }
+
---
+++ My project/Assets/sciptableObjects/Stage/Rooms.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Rooms")]
+
+
+
+public class Rooms : ScriptableObject
+{
+    public GameObject BossRoom;
+    public GameObject EliteRoom;
+    public GameObject ShopRoom;
+    public GameObject ChestRoom;
+    public List<GameObject> EasyRooms;
+    public List<GameObject> NormRooms;
+    public List<GameObject> HardRooms;
+
+}
